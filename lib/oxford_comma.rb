f340400886ad6b197f145b0cda34abfe332e5e76
Jacob <jacobliem1@gmail.com>
2020-11-21 23:49:28 -0600

    commit tree 024b066928fc1a88c8bd619cca3d91885d04b584

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+ return array.join
 end
+
+def oxford_comma(array)
+    return array.join(" and ")
+end
+
+def oxford_comma(array)
+    if array.size == 1
+      return array[0]
+    elsif array.size == 2
+      return array.join(" and ")
+    else
+      return array[0..-2].join(', ') + ", and " + array[-1]
+    end
+  end
